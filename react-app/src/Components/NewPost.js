1a7071abc3f53ad3f108e418dd4fc10148f454af
Manke Luo <teresaluo1210@berkeley.edu>
2022-11-04 22:42:15 -0700

    finished hw 5

--- react-app/src/Components/NewPost.js
+++ react-app/src/Components/NewPost.js
@@ -1,7 +1,7 @@
-import {_________} from "react";
+import {useState} from "react";
 import axios from "axios";
 
-const NewPost = ({ _________ }) => {
+const NewPost = (props) => {
   const [id, setId] = useState();
   const [title, setTitle] = useState();
   const [body, setBody] = useState();
@@ -13,7 +13,7 @@ const NewPost = ({ _________ }) => {
       body
     })
 
-    axios.post(_________, _________).then(_________);
+    axios.post('http://localhost:3002/post', {id, title, body}).then(props.func);
   }
 
   return <div>
